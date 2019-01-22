2b2394e81f103c2df758d7dc7143cf3523e1d8e9
egoing <egoing@gmail.com>
2019-01-22 15:40:53 +0900

    url parsing

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var http = require('http');
-
-//create a server object:
+var url = require('url');
 http.createServer(function (req, res) {
-    res.write('<script>document.write(Math.random());</script>');
-    res.write('<br>');
-    res.write(Math.random().toString()); //write a response to the client
-    res.end(); //end the response
-}).listen(9999); //the server object listens on port 8080
+    var parsedUrl = url.parse(req.url, true);
+    var title = parsedUrl.query.id;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(`
+    <html>
+    <body>
+        <h1><a href="/index.html">WEB</a></h1>
+        <ol>
+            <li><a href="html.html">html</a></li>
+            <li><a href="css.html">css</a></li>
+            <li><a href="javascript.html">javascript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        HTML is Hypertext Markup Language. Velit pariatur reprehenderit id cupidatat officia adipisicing. Elit laboris
+        pariatur enim voluptate ut ex ut qui duis aliquip aute. Commodo ut exercitation irure pariatur occaecat tempor
+        ullamco
+    </body>
+    </html>
+    `); 
+    res.end();
+}).listen(9999);
